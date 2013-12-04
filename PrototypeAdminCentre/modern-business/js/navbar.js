6618826bf7fb814de093e03dbb1a9710e47bc15c
Anthony Free <marinu666@gmail.com>
2013-12-04 15:01:27 +0000

    Javascript for adding same urls to every page

---
+++ PrototypeAdminCentre/modern-business/js/navbar.js
@@ -0,0 +1,24 @@
+var navbar = {
+	init: function() {
+		try {
+			navbar.setupNavbar();
+		} catch(e) {
+			setTimeout('navbar.init()', 500);
+		}
+	},
+	
+	setupNavbar: function() {
+		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('about.html', 'About');
+		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('services.html', 'Services');
+		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('contact.php', 'Contact');
+		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('clients.html', 'Clients');
+		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('guide.html', 'Documentation');
+	},
+	
+	createli: function(hrefLink, hrefName) {
+		console.log("Adding link: " + hrefName + " => " + hrefLink);
+		return '<li><a href="' + hrefLink + '">' + hrefName + '</a>';
+	}
+}
+
+navbar.init();
