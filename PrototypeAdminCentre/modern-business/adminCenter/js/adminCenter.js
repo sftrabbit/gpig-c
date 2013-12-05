2e918e8af60bccb82d8305e93b620e9d273a2e8e
Anthony Free <marinu666@gmail.com>
2013-12-05 16:05:24 +0000

    added admin center js file

---
+++ PrototypeAdminCentre/modern-business/adminCenter/js/adminCenter.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var adminCenter = {
+    init: function() {
+        
+    },
+    setTitle: function(title) {
+        document.title = title + " :: GPIG-C";
+    }
+}
+
