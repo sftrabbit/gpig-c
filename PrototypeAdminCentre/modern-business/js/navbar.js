6194d028649c469c77186cfbbcc9f5d8b8ce4b50
Tom Davies <tod502@york.ac.uk>
2013-12-05 16:10:53 +0000

    Change the actual navbar.js to use contact.html. We had two!?

--- PrototypeAdminCentre/modern-business/js/navbar.js
+++ PrototypeAdminCentre/modern-business/js/navbar.js
@@ -6,15 +6,15 @@ var navbar = {
 			setTimeout('navbar.init()', 500);
 		}
 	},
-	
+
 	setupNavbar: function() {
 		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('services.html', 'Services');
-		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('contact.php', 'Contact');
+		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('contact.html', 'Contact');
 		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('clients.html', 'Clients');
 		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('guide.html', 'Documentation');
         document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createbutton('login.html', 'Log In');
     },
-	
+
 	createli: function(hrefLink, hrefName) {
 		console.log("Adding link: " + hrefName + " => " + hrefLink);
 		return '<li><a href="' + hrefLink + '">' + hrefName + '</a>';
