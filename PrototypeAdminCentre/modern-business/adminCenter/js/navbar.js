83d9e96297c4b5dea1dc40f02ce133588d535a9b
Rosy Tucker <rt615@york.ac.uk>
2013-12-05 14:45:07 +0000

    Convert admin navbar to jquery

--- PrototypeAdminCentre/modern-business/adminCenter/js/navbar.js
+++ PrototypeAdminCentre/modern-business/adminCenter/js/navbar.js
@@ -1,27 +1,18 @@
-var navbar = {
-	init: function() {
-		try {
-			navbar.setupNavbar();
-		} catch(e) {
-			setTimeout('navbar.init()', 500);
-		}
-	},
-	
-	setupNavbar: function() {
-		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('services.html', 'Services');
-		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('contact.php', 'Contact');
-		document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createli('guide.html', 'Documentation');
-        document.getElementById('nbarstart').innerHTML = document.getElementById('nbarstart').innerHTML + navbar.createbutton('../login.html', 'Log Out');
-    },
-	
-	createli: function(hrefLink, hrefName) {
-		console.log("Adding link: " + hrefName + " => " + hrefLink);
-		return '<li><a href="' + hrefLink + '">' + hrefName + '</a>';
-	},
-
-    createbutton: function(hrefLink, hrefName){
-        console.log("Adding link: " + hrefName + " => " + hrefLink);
-        return '<li><a href="' + hrefLink + '"><button  type="submit" class= "btn btn-less-padding">' + hrefName +'</button></a></li>'
-    }
+function createli (hrefLink, hrefName) {
+    return '<li><a href="' + hrefLink + '">' + hrefName + '</a>';
 }
-navbar.init();
+
+function createbutton(hrefLink, hrefName){
+    return '<li><a href="' + hrefLink + '">' +
+        '<button  type="submit" class= "btn btn-less-padding">' + hrefName +'</button></a></li>'
+}
+
+$(function() {
+    $('#nbarstart').html($('#nbarstart').html() + createli('services.html', 'Services'));
+    $('#nbarstart').html($('#nbarstart').html() + createli('contact.php', 'Contact'));
+    $('#nbarstart').html($('#nbarstart').html() + createli('guide.html', 'Documentation'));
+    $('#nbarstart').html($('#nbarstart').html() + createbutton('../login.html', 'Log Out'));
+} );
+	
+
+
