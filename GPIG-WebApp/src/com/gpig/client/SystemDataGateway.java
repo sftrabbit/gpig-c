a7668c3512d29cf49ebd8f3ae62caa3a36f41ad4
Tom Davies <tod502@york.ac.uk>
2014-01-29 21:50:46 +0000

    Add datastore interface

---
+++ GPIG-WebApp/src/com/gpig/client/SystemDataGateway.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.gpig.client;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * The interface to any datastore
+ * 
+ * @author Tom Davies
+ */
+public interface SystemDataGateway {
+	
+	/**
+	 * @param systemID A systemID
+	 * @param numRecords The maximum number of records to return
+	 * @return The numRecords most recent records associated with the given
+	 * systemID
+	 */
+	public List<SystemData> readMostRecent(String systemID, int numRecords);
+	
+	/**
+	 * @param systemID A systemID
+	 * @param start The earliest point in the time period
+	 * @param end The latest point in the time period
+	 * @return All records for a given systemID within the given time period
+	 */
+	public List<SystemData> readBetween(String systemID, Date start, Date end);
+	
+	/**
+	 * Writes the given data to the datastore
+	 * 
+	 * @param data The data to be written
+	 */
+	public void write(SystemData data);
+
+}
