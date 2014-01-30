211be926230fdfb6539094a2adc7201bf64cd885
Joseph Mansfield <sftrabbit@gmail.com>
2014-01-30 22:49:11 +0000

    Document ProtoSender

--- DataEmitter/src/com/gpigc/dataemitter/ProtoSender.java
+++ DataEmitter/src/com/gpigc/dataemitter/ProtoSender.java
@@ -6,32 +6,56 @@ import java.net.Socket;
 
 import com.gpigc.proto.Protos.SystemData;
 
+/**
+ * Sends Protocol Buffer messages via a socket.
+ */
 public class ProtoSender {
 	protected final Socket sock;
 	protected final OutputStream os;
-	
-	/* Construction */
-	public ProtoSender(final Socket sock) throws IOException
-	{
+
+	/**
+	 * Send Protocol Buffer messages via the given socket.
+	 * 
+	 * @param sock
+	 *            Socket to send messages through.
+	 * @throws IOException
+	 */
+	public ProtoSender(final Socket sock) throws IOException {
 		this.sock = sock;
 		this.os = sock.getOutputStream();
 	}
-	
-	public ProtoSender(String hostname, int port) throws IOException
-	{
+
+	/**
+	 * Send Protocol Buffer messages to the given hostname and port.
+	 * 
+	 * @param hostname
+	 *            Hostname to send messages to
+	 * @param port
+	 *            Port to send messages to
+	 * @throws IOException
+	 */
+	public ProtoSender(String hostname, int port) throws IOException {
 		this(new Socket(hostname, port));
 	}
-	
-	/* Send the message through the socket */
-	public void send(SystemData msg) throws IOException
-	{
+
+	/**
+	 * Send a SystemData message.
+	 * 
+	 * @param msg
+	 *            Message to send
+	 * @throws IOException
+	 */
+	public void send(SystemData msg) throws IOException {
 		msg.writeDelimitedTo(os);
 		os.flush();
 	}
-	
-	/* Close the connection - call before destruction */
-	public void close() throws IOException
-	{
+
+	/**
+	 * Close the socket.
+	 * 
+	 * @throws IOException
+	 */
+	public void close() throws IOException {
 		os.close();
 		sock.close();
 	}
