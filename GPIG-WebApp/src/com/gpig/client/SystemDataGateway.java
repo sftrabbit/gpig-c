57fe5cc2bf7de32b880e706d1ec81c8f0f08cf6d
Michael Walker <mike@barrucadu.co.uk>
2014-01-30 23:49:52 +0000

    Make SystemDataGateway only throw FailedTo{ReadFrom,WriteTo}DatastoreExceptions, to hide implementation details.

--- GPIG-WebApp/src/com/gpig/client/SystemDataGateway.java
+++ GPIG-WebApp/src/com/gpig/client/SystemDataGateway.java
@@ -3,12 +3,7 @@
  */
 package com.gpig.client;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
 import java.util.Date;
-
-import org.apache.http.ParseException;
 
 /**
  * The interface to any datastore
@@ -22,36 +17,27 @@ public interface SystemDataGateway {
 	 * @param numRecords The maximum number of records to return
 	 * @return The numRecords most recent records associated with the given
 	 * systemID
-	 * @throws FailedToReadFromDatastoreException 
-	 * @throws IOException 
-	 * @throws ParseException 
-	 * @throws URISyntaxException 
+	 * @throws FailedToReadFromDatastoreException
 	 */
 	public QueryResult readMostRecent(String systemID, int numRecords) 
-			throws FailedToReadFromDatastoreException, URISyntaxException, ParseException, IOException;
+			throws FailedToReadFromDatastoreException;
 	
 	/**
 	 * @param systemID A systemID
 	 * @param start The earliest point in the time period
 	 * @param end The latest point in the time period
 	 * @return All records for a given systemID within the given time period
-	 * @throws FailedToReadFromDatastoreException 
-	 * @throws URISyntaxException 
-	 * @throws IOException 
-	 * @throws ParseException 
+	 * @throws FailedToReadFromDatastoreException
 	 */
 	public QueryResult readBetween(String systemID, Date start, Date end) 
-			throws FailedToReadFromDatastoreException, URISyntaxException, ParseException, IOException;
+			throws FailedToReadFromDatastoreException;
 	
 	/**
 	 * Writes the given data to the datastore
 	 * 
 	 * @param data The data to be written
 	 * @throws FailedToWriteToDatastoreException When we couldn't write to the
-	 * database
-	 * @throws IOException 
-	 * @throws UnsupportedEncodingException 
 	 */
-	public void write(EmitterSystemState data) throws FailedToWriteToDatastoreException, UnsupportedEncodingException, IOException;
+	public void write(EmitterSystemState data) throws FailedToWriteToDatastoreException;
 
 }
