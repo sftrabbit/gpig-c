1838a5aacd1c481d0aea71ab8cb055f530cc5980
Joseph Mansfield <sftrabbit@gmail.com>
2014-01-30 21:51:25 +0000

    Tidy up TestAppEmitter

--- DataEmitter/src/com/gpigc/dataemitter/TestAppEmitter.java
+++ DataEmitter/src/com/gpigc/dataemitter/TestAppEmitter.java
@@ -6,22 +6,23 @@ public class TestAppEmitter {
 	private static final String TEST_APP_NAME = "b.jar";
 
 	public static void main(String[] args) throws Exception {
-		JavaVirtualMachineMonitor jvm = new JavaVirtualMachineMonitor(
-				TEST_APP_NAME);
-		long pid = jvm.getProcessId();
-
-		ProcessMonitor slm = new ProcessMonitor(pid);
-		Thread.sleep(4500);
-
 		ProtoSender sender = new ProtoSender();
+		
+		JavaVirtualMachineMonitor jvmMonitor = new JavaVirtualMachineMonitor(
+				TEST_APP_NAME);
+		long pid = jvmMonitor.getProcessId();
+
+		ProcessMonitor processMonitor = new ProcessMonitor(pid);
+		Thread.sleep(4500);
+		
 		try {
 			while (true) {
 				SystemData.Datum cpuDatum = SystemData.Datum.newBuilder()
-						.setKey("CPU").setValue(String.valueOf(slm.getLoad()))
+						.setKey("CPU").setValue(String.valueOf(processMonitor.getLoad()))
 						.build();
 				SystemData.Datum memoryDatum = SystemData.Datum.newBuilder()
 						.setKey("Mem")
-						.setValue(String.valueOf(jvm.getUsedMemory())).build();
+						.setValue(String.valueOf(jvmMonitor.getUsedMemory())).build();
 				SystemData data = SystemData.newBuilder().setSystemId("1")
 						.setTimestamp(System.nanoTime()).addDatum(cpuDatum)
 						.addDatum(memoryDatum).build();
