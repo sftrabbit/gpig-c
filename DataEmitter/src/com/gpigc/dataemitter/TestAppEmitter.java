ccf9898ebdc144b9c75624de494bfc934f5b70df
Joseph Mansfield <sftrabbit@gmail.com>
2014-01-30 17:21:21 +0000

    Reformat TestAppEmitter main

--- DataEmitter/src/com/gpigc/dataemitter/TestAppEmitter.java
+++ DataEmitter/src/com/gpigc/dataemitter/TestAppEmitter.java
@@ -4,34 +4,34 @@ import com.gpigc.proto.Protos.SystemData;
 
 public class TestAppEmitter {
 	private static final String TEST_APP_NAME = "b.jar";
-	
+
 	public static void main(String[] args) throws Exception {
 		JvmHook jvmHook = new JvmHook(TEST_APP_NAME);
 		long pid = jvmHook.getProcessId();
-		
-		ProtoSender sender = new ProtoSender();
 
-        SigarLoadMonitor slm = new SigarLoadMonitor(pid);
-        Thread.sleep(4500);
-        try {
-	        while (true) {
-	        	SystemData data = SystemData.newBuilder()
-	        		.setSystemId("1")
-	        		.setTimestamp(System.nanoTime())
-	        		.addDatum(SystemData.Datum.newBuilder()
-	        			.setKey("CPU")
-	        			.setValue(String.valueOf(slm.getLoad())))
-	        		.addDatum(SystemData.Datum.newBuilder()
-	        			.setKey("Mem")
-	        			.setValue(String.valueOf(jvmHook.getUsedMemory())))
-	        		.build();
-	        	
-	        	sender.send(data);
-	        	
-	        	Thread.sleep(1000);
-	        }
-        } catch (Exception e) {
-        	System.out.println("Server closed socket.");
-        }
+		SigarLoadMonitor slm = new SigarLoadMonitor(pid);
+		Thread.sleep(4500);
+
+		ProtoSender sender = new ProtoSender();
+		try {
+			while (true) {
+				SystemData.Datum cpuDatum = SystemData.Datum.newBuilder()
+						.setKey("CPU").setValue(String.valueOf(slm.getLoad()))
+						.build();
+				SystemData.Datum memoryDatum = SystemData.Datum.newBuilder()
+						.setKey("Mem")
+						.setValue(String.valueOf(jvmHook.getUsedMemory()))
+						.build();
+				SystemData data = SystemData.newBuilder().setSystemId("1")
+						.setTimestamp(System.nanoTime()).addDatum(cpuDatum)
+						.addDatum(memoryDatum).build();
+
+				sender.send(data);
+
+				Thread.sleep(1000);
+			}
+		} catch (Exception e) {
+			System.out.println("Server closed socket.");
+		}
 	}
 }
