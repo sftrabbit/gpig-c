2bb55c53302a23aa64a66beca1527e3714711c70
Joseph Mansfield <sftrabbit@gmail.com>
2014-01-30 01:11:28 +0000

    Detect when test app is not running

--- DataEmitter/src/com/gpigc/dataemitter/TestAppEmitter.java
+++ DataEmitter/src/com/gpigc/dataemitter/TestAppEmitter.java
@@ -5,42 +5,50 @@ import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 
 public class TestAppEmitter {
-	public static void main(String[] args) throws Exception
-	{
-		long pid = 0;
+	private static long getTestAppPid() {
         for (VirtualMachineDescriptor descriptor : VirtualMachine.list()) {
             if (descriptor.displayName().equals("b.jar")) {
-            	pid = Long.parseLong(descriptor.id());
+            	return Long.parseLong(descriptor.id());
             }
         }
         
-		Attach att = new Attach(pid);
-		
-		ProtoSender sender = new ProtoSender();
-
-        SigarLoadMonitor slm = new SigarLoadMonitor(pid);
-        Thread.sleep(4500);
-        try
-        {
-	        while(true)
+        return 0;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+        long pid = getTestAppPid();
+        if (pid > 0) {
+			Attach att = new Attach(pid);
+			
+			ProtoSender sender = new ProtoSender();
+	
+	        SigarLoadMonitor slm = new SigarLoadMonitor(pid);
+	        Thread.sleep(4500);
+	        try
 	        {
-	        	SystemData data = SystemData.newBuilder()
-	        		.setSystemId("1")
-	        		.setTimestamp(System.nanoTime())
-	        		.addDatum(SystemData.Datum.newBuilder()
-	        			.setKey("CPU")
-	        			.setValue(String.valueOf(slm.getLoad())))
-	        		.addDatum(SystemData.Datum.newBuilder()
-	        			.setKey("Mem")
-	        			.setValue(String.valueOf(att.getmem())))
-	        		.build();
-	        	
-	        	sender.send(data);
-	        	
-	        	Thread.sleep(1000);
+		        while(true)
+		        {
+		        	SystemData data = SystemData.newBuilder()
+		        		.setSystemId("1")
+		        		.setTimestamp(System.nanoTime())
+		        		.addDatum(SystemData.Datum.newBuilder()
+		        			.setKey("CPU")
+		        			.setValue(String.valueOf(slm.getLoad())))
+		        		.addDatum(SystemData.Datum.newBuilder()
+		        			.setKey("Mem")
+		        			.setValue(String.valueOf(att.getmem())))
+		        		.build();
+		        	
+		        	sender.send(data);
+		        	
+		        	Thread.sleep(1000);
+		        }
+	        } catch (Exception e) {
+	        	System.out.println("Server closed socket.");
 	        }
-        } catch (Exception e) {
-        	System.out.println("Server closed socket.");
-        }
+        } else {
+	    	System.out.println("Test application not running");
+	    }
 	}
 }
