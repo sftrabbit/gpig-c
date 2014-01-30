20627b1533bb008a242204bbc4553af90264b5b9
Tom Davies <tod502@york.ac.uk>
2014-01-30 16:05:46 +0000

    Remove unused import

--- GPIG-WebApp/src/com/gpig/server/AppEngineServlet.java
+++ GPIG-WebApp/src/com/gpig/server/AppEngineServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletException;
