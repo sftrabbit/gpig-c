9c04e3ec4cbcb37a3b14f5ea5efdc1d4df38ede8
Tom Davies <tod502@york.ac.uk>
2014-02-07 18:26:32 +0000

    Add (blank) ExpressionAnalysisTest

---
+++ projects/Core/test/com/gpigc/test/ExpressionAnalysisTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.gpigc.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author GPIG-C
+ */
+public class ExpressionAnalysisTest {
+
+	/**
+	 * Test method for {@link com.gpigc.core.analysis.engine.ExpressionAnalysis#analyse()}.
+	 */
+	@Test
+	public void testAnalyse() {
+		fail("Not yet implemented");
+	}
+
+}
