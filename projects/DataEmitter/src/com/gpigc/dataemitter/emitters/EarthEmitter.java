d21ded07b0189a68b85d6e453a961cd1317e8935
Joseph Mansfield <sftrabbit@gmail.com>
2014-02-08 13:42:24 +0000

    Build earthquake SystemData

--- projects/DataEmitter/src/com/gpigc/dataemitter/emitters/EarthEmitter.java
+++ projects/DataEmitter/src/com/gpigc/dataemitter/emitters/EarthEmitter.java
@@ -1,6 +1,9 @@
 package com.gpigc.dataemitter.emitters;
 
+import java.util.List;
+
 import com.gpigc.dataemitter.monitors.EarthquakeMonitor;
+import com.gpigc.dataemitter.monitors.EarthquakeMonitor.Earthquake;
 import com.gpigc.proto.Protos.SystemData;
 
 public class EarthEmitter extends Emitter {
@@ -19,6 +22,18 @@ public class EarthEmitter extends Emitter {
 
 	@Override
 	public SystemData collectData() throws Exception {
-		return null;
+		SystemData.Builder dataBuilder = SystemData.newBuilder().setSystemId("2")
+				.setTimestamp(System.nanoTime());
+		
+		List<Earthquake> earthquakes = earthquakeMonitor.getNewEarthquakes();
+		for (Earthquake earthquake : earthquakes) {
+			SystemData.Datum earthquakeDatum = SystemData.Datum.newBuilder()
+					.setKey("EQ")
+					.setValue(earthquake.toString())
+					.build();
+			dataBuilder.addDatum(earthquakeDatum);
+		}
+		
+		return dataBuilder.build();
 	}
 }
--- projects/DataEmitter/src/com/gpigc/dataemitter/monitors/EarthquakeMonitor.java
+++ projects/DataEmitter/src/com/gpigc/dataemitter/monitors/EarthquakeMonitor.java
@@ -1,5 +1,16 @@
 package com.gpigc.dataemitter.monitors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EarthquakeMonitor {
+
+	public class Earthquake {
+
+	}
+
+	public List<Earthquake> getNewEarthquakes() {
+		return new ArrayList<Earthquake>();
+	}
 
 }
