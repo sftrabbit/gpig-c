6c5f8bea1def902c6db29e5e5c55dba12ec55f01
Joseph Mansfield <sftrabbit@gmail.com>
2014-02-08 14:51:04 +0000

    Fix _StubEmitter_ errors

--- projects/DataEmitter/src/com/gpigc/dataemitter/emitters/_StubEmitter_.java
+++ projects/DataEmitter/src/com/gpigc/dataemitter/emitters/_StubEmitter_.java
@@ -4,15 +4,16 @@ import com.gpigc.proto.Protos.SystemData;
 
 public class _StubEmitter_ extends Emitter {
 
+	public _StubEmitter_() {
+		super(0);
+	}
+
 	@Override
-	public Void setup() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public void setup() throws Exception {
 	}
 
 	@Override
 	public SystemData collectData() throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
