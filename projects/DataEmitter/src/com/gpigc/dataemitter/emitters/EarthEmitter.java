4c8a7ecd19f72e86ff3f0be858922d6c2c3ecd6f
Joseph Mansfield <sftrabbit@gmail.com>
2014-02-08 12:46:44 +0000

    Add an EarthEmitter stub

---
+++ projects/DataEmitter/src/com/gpigc/dataemitter/emitters/EarthEmitter.java
@@ -0,0 +1,18 @@
+package com.gpigc.dataemitter.emitters;
+
+import com.gpigc.proto.Protos.SystemData;
+
+public class EarthEmitter extends Emitter {
+
+	@Override
+	public Void setup() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SystemData collectData() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
