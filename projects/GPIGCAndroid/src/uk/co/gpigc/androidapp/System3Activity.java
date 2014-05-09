1034b95e075bf3a906fe8fd95626b330a0afd3fe
Tom Davies <tod502@york.ac.uk>
2014-05-09 16:05:34 +0100

    Add OpenCV to Android project (OMG - SORRAY ABOUT THE SIZE!!!)

--- projects/GPIGCAndroid/src/uk/co/gpigc/androidapp/System3Activity.java
+++ projects/GPIGCAndroid/src/uk/co/gpigc/androidapp/System3Activity.java
@@ -1,16 +1,27 @@
 package uk.co.gpigc.androidapp;
 
-import uk.co.gpigc.gpigcandroid.R;
+import org.opencv.android.OpenCVLoader;
 
+import uk.co.gpigc.gpigcandroid.R;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class System3Activity extends Activity {
+	
+	static {
+	    startOpenCV();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_system_face);
+	}
+
+	private static void startOpenCV() {
+		if (!OpenCVLoader.initDebug()) {
+	        // Handle initialization error
+	    }
 	}
 }
 
