c8c805a1467020ed5494f36058e4ce8da7847aa0
Tom Davies <tod502@york.ac.uk>
2014-05-09 16:26:12 +0100

    Add OpenCV boilerplate for showing frames from the camera and collecting them for processing

--- projects/GPIGCAndroid/src/uk/co/gpigc/androidapp/System3Activity.java
+++ projects/GPIGCAndroid/src/uk/co/gpigc/androidapp/System3Activity.java
@@ -1,12 +1,22 @@
 package uk.co.gpigc.androidapp;
 
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
 import uk.co.gpigc.gpigcandroid.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class System3Activity extends Activity {
+public class System3Activity extends Activity implements CvCameraViewListener2 {
+	
+	private static int REAR_FACING_CAMERA = 0;
+	
+	private FrameLayout mFrameLayoutCamera;
+	private JavaCameraView cameraView;
 	
 	static {
 	    startOpenCV();
@@ -16,12 +26,47 @@ public class System3Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_system_face);
+		mFrameLayoutCamera = (FrameLayout) findViewById(R.id.frameLayoutLiveView);
+		cameraView = new JavaCameraView(
+				getApplicationContext(), REAR_FACING_CAMERA);
+		cameraView.setCvCameraViewListener(this);
+		mFrameLayoutCamera.addView(cameraView);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		cameraView.enableView();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		cameraView.disableView();
 	}
 
 	private static void startOpenCV() {
 		if (!OpenCVLoader.initDebug()) {
 	        // Handle initialization error
 	    }
+	}
+
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCameraViewStopped() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		// TODO Handle frames from camera on a different thread
+		return null;
 	}
 }
 
