e467fddba9c4daf74be44c0ec73368f7273fca9b
Tom Davies <tod502@york.ac.uk>
2014-05-09 15:46:12 +0100

    Make basic layout for the new face detection acitivty

--- projects/GPIGCAndroid/src/uk/co/gpigc/androidapp/System3Activity.java
+++ projects/GPIGCAndroid/src/uk/co/gpigc/androidapp/System3Activity.java
@@ -1,11 +1,5 @@
 package uk.co.gpigc.androidapp;
 
-
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphViewDataInterface;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
-
 import uk.co.gpigc.gpigcandroid.R;
 
 import android.app.Activity;
@@ -18,43 +12,6 @@ public class System3Activity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_system2);
-
-		// init example series data
-		GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
-				new GraphViewData(1, 2.0d)
-				, new GraphViewData(2, 1.5d)
-				, new GraphViewData(3, 2.5d)
-				, new GraphViewData(4, 1.0d)
-		});
-
-		GraphView graphView = new LineGraphView(
-				this // context
-				, "GraphViewDemo" // heading
-				);
-		graphView.addSeries(exampleSeries); // data
-
-		RelativeLayout layout = (RelativeLayout) findViewById(R.id.contentLayout);
-		layout.addView(graphView);
-	}
-
-	class GraphViewData implements GraphViewDataInterface {
-		private double x,y;
-
-		public GraphViewData(double x, double y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		@Override
-		public double getX() {
-			return this.x;
-		}
-
-		@Override
-		public double getY() {
-			return this.y;
-		}
-	}
+		setContentView(R.layout.activity_system_face);
 }
 
