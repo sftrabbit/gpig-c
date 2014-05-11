e3e3a07199e4bdac23e355edb6296a5236484fff
Tom Davies <tod502@york.ac.uk>
2014-05-11 18:54:08 +0100

    Add OpenCV and SWT stuff to Eclipse classpath to suppress build errors

--- projects/OpenCV-2.4.9-android/sdk/java/gen/org/opencv/engine/OpenCVEngineInterface.java
+++ projects/OpenCV-2.4.9-android/sdk/java/gen/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Rosy\\Documents\\GitHub\\gpig-c\\projects\\OpenCV-2.4.9-android\\sdk\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /Users/Tom/Documents/GPIG/Git/gpig-c/projects/OpenCV-2.4.9-android/sdk/java/src/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
