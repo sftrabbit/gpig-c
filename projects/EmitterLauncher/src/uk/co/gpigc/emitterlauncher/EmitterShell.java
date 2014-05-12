ee176a9fb3386dbf0b43ed6496ae571af3a7dcef
Rosy Tucker <rt615@york.ac.uk>
2014-05-12 19:59:07 +0100

    Emitter launcher Tabs ah yeahhhh
    Links are still dodgy to the jars

--- projects/EmitterLauncher/src/uk/co/gpigc/emitterlauncher/EmitterShell.java
+++ projects/EmitterLauncher/src/uk/co/gpigc/emitterlauncher/EmitterShell.java
@@ -8,7 +8,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.wb.swt.SWTResourceManager;
@@ -18,17 +17,28 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.custom.StyledText;
 
 public class EmitterShell extends Shell {
 
 	private static final String ICON_PATH = "/images/nosql.png";
 	private static final String STOP_BUTTON_PATH = "/images/control_stop_blue.png";
 	private static final String PLAY_BUTTON_PATH = "/images/control_play_blue.png";
-	
-	private static final String TEST_APP_1_PATH = "../TestApp1Emitter/bin/TestApp1Emitter.jar";
-	private static final String TEST_APP_2_PATH = "../TestApp2Emitter/bin/TestApp2Emitter.jar";
-	private static final String EARTH_APP_PATH = "../EarthEmitter/bin/EarthEmitter.jar";
-	private static final String SERVER_APP_PATH = "../ServerEmitter/bin/ServerEmitter.jar";
+
+	//private static final String TEST_APP_1_PATH = "../TestApp1Emitter/bin/TestApp1Emitter.jar";
+	private static final String TEST_APP_1_PATH = "../../emitterJars/TestApp1Emitter.jar";
+
+	//private static final String TEST_APP_2_PATH = "../TestApp2Emitter/bin/TestApp2Emitter.jar";
+	private static final String TEST_APP_2_PATH = "../../emitterJars/TestApp2Emitter.jar";
+
+	//private static final String EARTH_APP_PATH = "../EarthEmitter/bin/EarthEmitter.jar";
+	private static final String EARTH_APP_PATH = "../../emitterJars/EarthMonitor.jar";
+
+	//private static final String SERVER_APP_PATH = "../ServerEmitter/bin/ServerEmitter.jar";
+	private static final String SERVER_APP_PATH = "../../emitterJars/ServerEmitter.jar";
+
 
 	private Button serverButton;
 	private Button earthButton;
@@ -38,6 +48,16 @@ public class EmitterShell extends Shell {
 	private static Image stopIcon;
 	private Image shellIcon;
 	private final Map<String, OpenJarThread> threadMap = new HashMap<>();
+	private TabFolder tabFolder;
+	private TabItem console1Tab;
+	private TabItem console2Tab;
+	private StyledText textApp2;
+	private StyledText textApp1;
+	private TabItem console3Tab;
+	private StyledText textApp3;
+	private StyledText styledText;
+	private TabItem console4Tab;
+	private StyledText textApp4;
 
 	/**
 	 * Launch the application.
@@ -64,6 +84,40 @@ public class EmitterShell extends Shell {
 		shellSetup(display);
 		Group groupComp = groupSetup();
 		compositeSetup(groupComp);
+
+		tabFolder = new TabFolder(this, SWT.NONE);
+		GridData data = new GridData(SWT.LEFT, SWT.CENTER, true, true, 1, 1);
+		data.horizontalAlignment = SWT.FILL;
+		data.verticalAlignment = SWT.FILL;
+		tabFolder.setLayoutData(data);
+
+		console1Tab = new TabItem(tabFolder, SWT.NONE);
+		console1Tab.setText("Console 1 ");
+
+		textApp1 = new StyledText(tabFolder, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		textApp1.setEditable(false);
+		console1Tab.setControl(textApp1);
+
+		console2Tab = new TabItem(tabFolder, SWT.NONE);
+		console2Tab.setText("Console 2");
+
+		textApp2 = new StyledText(tabFolder, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		textApp2.setEditable(false);
+		console2Tab.setControl(textApp2);
+		
+		console3Tab = new TabItem(tabFolder, 0);
+		console3Tab.setText("Console 3");
+		
+		textApp3 = new StyledText(tabFolder, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		textApp3.setEditable(false);
+		console3Tab.setControl(textApp3);
+		
+		console4Tab = new TabItem(tabFolder, SWT.NONE);
+		console4Tab.setText("Console 4");
+		
+		textApp4 = new StyledText(tabFolder, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		textApp4.setEditable(false);
+		console4Tab.setControl(textApp4);
 	}
 
 	private void graphicsSetup(Display display) {
@@ -93,7 +147,7 @@ public class EmitterShell extends Shell {
 		testApp1Button.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				buttonSelect(TEST_APP_1_PATH, testApp1Button);
+				buttonSelect(TEST_APP_1_PATH, testApp1Button,textApp1);
 			}
 		});
 
@@ -107,7 +161,7 @@ public class EmitterShell extends Shell {
 		testApp2Button.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				buttonSelect(TEST_APP_2_PATH, testApp2Button);
+				buttonSelect(TEST_APP_2_PATH, testApp2Button, textApp2);
 			}
 		});
 
@@ -121,7 +175,7 @@ public class EmitterShell extends Shell {
 		earthButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				buttonSelect(EARTH_APP_PATH, earthButton);
+				buttonSelect(EARTH_APP_PATH, earthButton, textApp3);
 			}
 		});
 
@@ -135,15 +189,15 @@ public class EmitterShell extends Shell {
 		serverButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				buttonSelect(SERVER_APP_PATH, serverButton);
+				buttonSelect(SERVER_APP_PATH, serverButton, textApp4);
 			}
 		});
 	}
 
-	protected void buttonSelect(final String jarPath, Button button) {
+	protected void buttonSelect(final String jarPath, Button button, StyledText console) {
 		if (button.getSelection()) {
 			button.setImage(stopIcon);
-			threadMap.put(jarPath, new OpenJarThread(jarPath, button));
+			threadMap.put(jarPath, new OpenJarThread(jarPath, button,console));
 			threadMap.get(jarPath).start();
 		} else {
 			button.setImage(playIcon);
@@ -163,7 +217,13 @@ public class EmitterShell extends Shell {
 	}
 
 	private Group groupSetup() {
+		setLayout(new GridLayout(1, false));
 		Group group = new Group(this, SWT.NONE);
+		GridData groupData  = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		groupData.horizontalAlignment = SWT.FILL;
+		groupData.verticalAlignment = SWT.FILL;
+		group.setLayoutData(groupData);
+
 		group.setText("Run");
 		group.setLayout(new GridLayout(1, false));
 
@@ -173,7 +233,7 @@ public class EmitterShell extends Shell {
 		lblNewLabel.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblNewLabel.setAlignment(SWT.CENTER);
 		lblNewLabel
-				.setText("\nClick to run the selected emitter, click again to stop. \n ");
+		.setText("\nClick to run the selected emitter, click again to stop. \n ");
 		GridData labelData = new GridData(SWT.LEFT, SWT.TOP, true, false, 1, 1);
 		labelData.horizontalAlignment = SWT.FILL;
 		labelData.verticalAlignment = SWT.FILL;
@@ -192,10 +252,6 @@ public class EmitterShell extends Shell {
 		setSize(400, 300);
 		setMinimumSize(400, 200);
 		setText("GPIG-C: Launcher");
-		FillLayout fillLayout = new FillLayout(SWT.HORIZONTAL);
-		fillLayout.marginWidth = 10;
-		fillLayout.marginHeight = 10;
-		setLayout(fillLayout);
 	}
 
 	@Override
--- projects/EmitterLauncher/src/uk/co/gpigc/emitterlauncher/OpenJarThread.java
+++ projects/EmitterLauncher/src/uk/co/gpigc/emitterlauncher/OpenJarThread.java
@@ -1,5 +1,11 @@
 package uk.co.gpigc.emitterlauncher;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 
@@ -8,32 +14,49 @@ public class OpenJarThread extends Thread {
 	private Process proc;
 	private String jarPath;
 	private Button button;
+	private StyledText console;
 
-	public OpenJarThread(String jarPath, Button button) {
+	public OpenJarThread(String jarPath, Button button, StyledText console) {
 		this.jarPath = jarPath;
 		this.button = button;
+		this.console = console;
 	}
 
 	@Override
 	public void run() {
 		try {
 			proc = Runtime.getRuntime().exec("java -jar "+jarPath);
+			redirectOutput();
 			proc.waitFor();
 			stopRunning();
 			Display.getDefault().asyncExec(new Runnable() {
-				
+
 				@Override
 				public void run() {
 					button.setSelection(false);
 					EmitterShell.changeIcon(button);
 				}
 			});
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+
+	private void redirectOutput() {
+		final Scanner s = new Scanner(proc.getInputStream());
+		Display.getDefault().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				if(s.hasNextLine()){
+					console.append(s.nextLine());
+					console.append("\n");
+				}
+			}
+		});
+	}
 
 	public void stopRunning(){
 		if(proc != null)
