c244c4e6d056fcb8885cd0f85a601d1af0cc9b96
Joseph Mansfield <sftrabbit@gmail.com>
2014-05-12 16:18:43 +0100

    Add do-nothing Earthquake Emitter

---
+++ projects/EarthquakeEmitter/src/uk/co/gpigc/emitter/earthquake/EarthquakeCollector.java
@@ -0,0 +1,17 @@
+package uk.co.gpigc.emitter.earthquake;
+
+import java.util.List;
+
+import com.gpigc.proto.Protos.SystemData;
+
+import uk.co.gpigc.emitter.DataCollector;
+
+public class EarthquakeCollector implements DataCollector {
+
+	@Override
+	public List<SystemData> collect() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ projects/EarthquakeEmitter/src/uk/co/gpigc/emitter/earthquake/EarthquakeEmitter.java
@@ -0,0 +1,10 @@
+package uk.co.gpigc.emitter.earthquake;
+
+public class EarthquakeEmitter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
