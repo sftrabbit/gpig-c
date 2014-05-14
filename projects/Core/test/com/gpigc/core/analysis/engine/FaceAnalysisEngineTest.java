75381681efc71dfd82e9b96c7f98f13b78e3f060
Tom Davies <tod502@york.ac.uk>
2014-05-14 16:46:15 +0100

    Add test stibs for FaceAnalysisEngine

---
+++ projects/Core/test/com/gpigc/core/analysis/engine/FaceAnalysisEngineTest.java
@@ -0,0 +1,24 @@
+package com.gpigc.core.analysis.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FaceAnalysisEngineTest {
+
+	@Test
+	public void testParseFaces() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseFace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAuthorisedFace() {
+		fail("Not yet implemented");
+	}
+
+}
