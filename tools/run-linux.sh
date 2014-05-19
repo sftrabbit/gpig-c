481e7b1148d0ab521f3ff260d2e8ecd8cb435b07
Andrew Fairbairn <wave3k@gmail.com>
2014-05-19 18:11:27 +0100

    FIX FIX FIX

--- tools/run-linux.sh
+++ tools/run-linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -jar "$DIR/Core.jar" > /dev/null 2>&1 &
+java -Dorg.apache.jasper.compiler.disablejsr199=true -jar "$DIR/Core.jar" > /dev/null 2>&1 &
 java -jar "$DIR/EmitterLauncher.jar" > /dev/null 2>&1 &
