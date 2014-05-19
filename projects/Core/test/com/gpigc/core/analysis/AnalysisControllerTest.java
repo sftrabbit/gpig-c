497018ac51abf095ca14025828195a8e7cde0b3a
Joseph Mansfield <sftrabbit@gmail.com>
2014-05-19 11:20:15 +0100

    Fix Config errors in tests

--- projects/Core/test/com/gpigc/core/analysis/AnalysisControllerTest.java
+++ projects/Core/test/com/gpigc/core/analysis/AnalysisControllerTest.java
@@ -8,7 +8,10 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 import com.gpigc.core.ClientSystem;
+import com.gpigc.core.Config;
+import com.gpigc.core.Config.ConfigException;
 import com.gpigc.core.Core;
+import com.gpigc.core.Main;
 import com.gpigc.core.analysis.AnalysisController;
 
 public class AnalysisControllerTest {
@@ -17,10 +20,11 @@ public class AnalysisControllerTest {
 
 	@Test
 	public void testMakeEngines() throws ReflectiveOperationException,
-			IOException, InterruptedException {
+			IOException, InterruptedException, ConfigException {
+		Config config = new Config(Main.CONFIG_NAME);
 		AnalysisController analysisController = new AnalysisController(
 				new ArrayList<ClientSystem>(), new Core(
-						"config/RegisteredSystems.config")); // no
+						"config/RegisteredSystems.config", config)); // no
 																// exception
 																// thrown
 		assertNotNull(analysisController.getAnalysisEngines());
@@ -29,10 +33,11 @@ public class AnalysisControllerTest {
 
 	@Test
 	public void testSystemUpdate() throws ReflectiveOperationException,
-			IOException, InterruptedException {
+			IOException, InterruptedException, ConfigException {
+		Config config = new Config(Main.CONFIG_NAME);
 		AnalysisController analysisController = new AnalysisController(
 				new ArrayList<ClientSystem>(), new Core(
-						"config/RegisteredSystems.config")); // no
+						"config/RegisteredSystems.config", config)); // no
 																// exception
 																// thrown
 		analysisController.analyse(systemID);
